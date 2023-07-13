dd17f062a8f78602228be5266314eef4d3007fa1
AlexDelei <alexdelei125@gmail.com>
2023-07-13 04:56:53 -0700

    First C coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #!/bin/bash
-int puts(){
-	return("\"Programming is like building a multilingual puzzle\");
+#include <stdio.h>
+int main(){
+	puts("Programming is like building a multilingual puzzle");
 		return 0;
 }
 	
