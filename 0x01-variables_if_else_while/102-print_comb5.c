f67f58435414362a554d791c2c932bea79bed5ef
AlexDelei <alexdelei125@gmail.com>
2023-07-14 08:06:33 -0700

    Combination5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints "Combination of all two-digit numbers"
+ * Return:Success value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		for (j = 1; j <= 99; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i != 0 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
