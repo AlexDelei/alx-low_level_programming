3af6bae4ccb59feef1115bee3f1027443e1a60df
AlexDelei <alexdelei125@gmail.com>
2023-07-14 05:47:43 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints "numbers of base 10"
+ * Return:Success value 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d\n", i)
+	}
+	return (0);
+}
