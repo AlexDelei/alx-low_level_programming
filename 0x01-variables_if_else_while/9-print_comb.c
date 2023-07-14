3675465122c14df5a92f65c2fe67a7d94415af0f
AlexDelei <alexdelei125@gmail.com>
2023-07-14 06:52:55 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,16 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			putchar('0' + i);
-			putchar('0' + j);
+		putchar('0' + i);
 
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-			}
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
