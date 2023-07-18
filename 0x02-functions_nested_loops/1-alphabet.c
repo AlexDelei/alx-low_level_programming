936947a03e1b408b2ca1958bfab22fddb4025c37
AlexDelei <alexdelei125@gmail.com>
2023-07-18 03:34:52 -0700

    Lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Success value 0
+ */
+int main(void)
+{
+	int _putchar(char c);
+	
+	{
+		return putchar(c);
+	}
+	return (0);
+}
