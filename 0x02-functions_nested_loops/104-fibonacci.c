38e0f341a893300e14050153666601928506f0c5
AlexDelei <alexdelei125@gmail.com>
2023-07-19 01:30:13 -0700

    Fibonacci last numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *fibonacci_sequence - This function will generate and print fibonacci numbers
+ *@a:stores the first last number
+ *@b:stores the last last number
+ *
+ *main - we call the fibonacci_sequence
+ */
+void fibonacci_sequence(int limit)
+{
+	int a = 1, b = 2;
+	int count;
+
+	while (count < limit)
+	{
+		if (count < limit - 1)
+		{
+			printf("%d, ", a);
+		}
+		else
+		{
+			printf("%d\n", a);
+		}
+
+		int temp = b;
+		b = a + b;
+		a = temp;
+		count++;
+	}
+}
+int main(void)
+{
+	fibonacci_sequence(98);
+	return (0);
+}
