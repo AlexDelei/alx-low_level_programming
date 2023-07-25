e11bf50efd0767206834ece8b0813fa8dc57e5b7
AlexDelei <alexdelei125@gmail.com>
2023-07-25 05:59:57 -0700

    Passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,27 +6,27 @@
 
 /*Function to generate a random password*/
 
-/*void generatePassword(char *pswd, int len)*/
-/*{*/
-	/*int i;*/
+void generatePassword(char *password, int length)
+{
+	int i;
 
-	/*static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";*/
-	/*for (i = 0; i < len; i++)*/
-	/*{*/
-		/*int index = rand() % (sizeof(charset) - 1);*/
-		/*pswd[i] = charset[index];*/
-	/*}*/
-	/*pswd[len] = '\0';*/
-/*}*/
+	static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	for (i = 0; i < length; i++)
+	{
+		int index = rand() % (sizeof(charset) - 1);
+		password[i] = charset[index];
+	}
+	password[length] = '\0';
+}
 int main()
 {
-	/*int pswdLength;*/
-	/*char pswd[MAX_PASSWORD_LENGTH + 1]*/;
+	int passwordLength;
+	char password[MAX_PASSWORD_LENGTH + 1];
 
-	/*srand((unsigned int)time(NULL));*/
-	/*pswdLength = 10;*/
+	srand((unsigned int)time(NULL));
+	passwordLength = 15;
 
-
-	printf("Tada! Congrats\n");
+	generatePassword(password, passwordLength);
+	printf("Tada! Congrats%d\n", passwordLength);
 	return (0);
 }
