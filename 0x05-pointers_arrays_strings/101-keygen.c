0351f781105593ae0616bdd9453535266f3d1a95
AlexDelei <alexdelei125@gmail.com>
2023-07-24 18:13:14 -0700

    Passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_PASSWORD_LENGTH
+
+/*Function to generate a random password*/
+
+void generatePassword(char *pswd, int len)
+{
+	static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	for (int i = 0; i < len; i++)
+	{
+		int index = rand() % (sizeof(charset) - 1);
+		pswd[i] = charset[index];
+	}
+	pswd[len] = '\0';
+}
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int passwordLength = 10;
+	char pswd[MAX_PASSWORD_LENGTH + 1];
+
+	generatePassword(pswd, passwordLength);
+	printf("Random Password is: %s\n", pswd);
+	return (0);
+}
