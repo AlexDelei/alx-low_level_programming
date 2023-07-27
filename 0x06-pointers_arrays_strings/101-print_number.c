47644ee7e146aebb7930b5f35c06d8cb40611860
AlexDelei <alexdelei125@gmail.com>
2023-07-27 11:59:42 -0700

    Printing numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,14 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
 	if (n < 0)
 	{
 		putchar('-');
-		n1 = -n;
+		n = -n;
 	}
-		if (n1 / 10 != 0)
-		{
-			print_number(n1 / 10);
-		}
-	putchar((n1 % 10) + '0');
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	putchar('0' + (n % 10));
 }
