447e78d281731a0e181bc4354ced043900bf14b9
AlexDelei <alexdelei125@gmail.com>
2023-07-31 06:12:37 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_memset _ fills the first bytes of the memory
+ * *s:Pointer to char variable
+ * b:Constant byte to be stored
+ * n:Number of bytes
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
