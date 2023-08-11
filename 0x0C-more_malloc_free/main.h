0dd99765ffce875139acf7106fe443c1989c35d9
AlexDelei <alexdelei125@gmail.com>
2023-08-11 13:34:40 -0700

    multiplying

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-
+int multiply(int num1, int num2);
+int main(int ac, char *av[]);
 
 #endif
