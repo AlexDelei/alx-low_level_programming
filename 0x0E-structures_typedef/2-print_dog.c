c651d8b6fd0a96c7b6e583d1801b60ca88448d9b
AlexDelei <alexdelei125@gmail.com>
2023-08-14 15:19:57 -0700

    ill teach you unconditional love.

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints struct dog
+ * @d: dog
+ *
+ *
+ */
+void print_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "{nil)");
+}
