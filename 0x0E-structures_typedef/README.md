6aa090f3a0b43e70c419448a45a527828dbff39e
AlexDelei <alexdelei125@gmail.com>
2023-08-14 13:41:06 -0700

    First structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+C strutures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - first structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: first structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
