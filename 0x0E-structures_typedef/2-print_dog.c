889c4cc3803426c9557c0b3c2372e1eda774457f
AlexDelei <alexdelei125@gmail.com>
2023-08-15 02:10:41 -0700

    ill teach you unconditional love.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,10 @@
  */
 void print_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
-
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.1f\n", (d->age) ? d->age : 0);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
