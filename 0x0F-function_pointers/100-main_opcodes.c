f239bb4c9383c3c55bb3ccfd2b2fcaeeccf56850
AlexDelei <alexdelei125@gmail.com>
2023-08-16 14:03:49 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opscode
+ * @ac: arguement size
+ * @av: arguement array
+ *
+ * Return: Nothing
+ */
+int main(int ac, char *av[])
+{
+	int numbyt;
+	unsigned char *mainPtr;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	numbyt = atoi(av[1]);
+	if (numbyt < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	mainPtr = (unsigned char *)main;
+	for (i = 0; i < numbyt; i++)
+	{
+		printf("%02x", mainPtr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
