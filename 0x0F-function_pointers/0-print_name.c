ec8912faf6394cb75558cbe8f96c8cad0b904c5a
AlexDelei <alexdelei125@gmail.com>
2023-08-16 05:25:17 -0700

    Function pointer call

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- *print - printing name
- *@name: the name
- *
- *
- */
-void print(char *name)
-{
-	printf("%s", name);
-}
-/**
  *print_name - printing name
  *@name: the name
  *@f: fuction pointer
