e6296f46cb88368bd4c259806233663e70c4bb2d
AlexDelei <alexdelei125@gmail.com>
2023-08-17 13:01:34 -0700

    Printing strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,10 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	const char *sep;
 	va_list ap;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(ap, n);
 	for (i = 0; i < n; ++i)
 	{
