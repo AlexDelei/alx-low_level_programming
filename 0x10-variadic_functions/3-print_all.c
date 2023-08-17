5273ce89792a11a7ce3fbcebba0989654409709e
AlexDelei <alexdelei125@gmail.com>
2023-08-17 15:04:49 -0700

    Anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints any arguement
+ * @format: data type format
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char c_arg;
+	int i_arg;
+	double f_arg;
+	char *s_arg;
+	va_list ap;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		if (i > 0 && format[i + 0])
+		{
+			printf(", ");
+		}
+		switch (format[i])
+		{
+			case 'c':
+				c_arg = (char)va_arg(ap, int);
+				printf("%c", c_arg);
+				break;
+
+			case 'i':
+				i_arg = va_arg(ap, int);
+				printf("%d", i_arg);
+				break;
+
+			case 'f':
+				f_arg = va_arg(ap, double);
+				printf("%f", f_arg);
+				break;
+
+			case 's':
+				s_arg = va_arg(ap, char*);
+				if (s_arg == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s_arg);
+					break;
+				}
+			default:
+				break;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
