8cd0c70718c9edffd70e18388e123079c03a1310
AlexDelei <alexdelei125@gmail.com>
2023-09-05 23:19:29 -0700

    Still weight 1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,57 @@
 #include "main.h"
 /**
- *error_exit - error output
- *@code: codechecker
- *@format: format
- *@arg: arguement
- */
-void error_exit(int code, const char *format, const char *arg)
-{
-	dprintf(STDERR_FILENO, format, arg);
-	exit(code);
-}
-/**
- *main - copies and prints
+ *main - copies content of a file to another
  *@argc: arguement count
- *@argv:arguement array
- *Return:0
+ *@argv: arguement array
+ *Return: 0 value
  */
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
- 	const char *file_to = argv[2];
+	const char *file_from = argv[1], *file_to = argv[2];
 	int fd_from, fd_to;
-	char buffer[1024];
+	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
 	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to file %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		exit(98);
 	}
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-
 		if (bytes_written == -1)
 		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to file %s\n", file_to);
-		}
-
-		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Write incomplete\n", NULL);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			exit(99);
 		}
 	}
-
-	if (bytes_read == -1) 
+	if (bytes_read == -1)
 	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+	if (close(fd_from) == -1)
 	{
-		error_exit(100, "Error: Can't close fd\n", NULL);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
-
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
