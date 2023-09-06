8519aa78a8a6fd5e7869c8efefc80350fe3e8efc
AlexDelei <alexdelei125@gmail.com>
2023-09-06 09:52:29 -0700

    Still weight 1

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,46 +1,48 @@
 #include "main.h"
 /**
- *main - displays the information contained in the ELF
+ *main - prints elf header info
  *@argc: arguement count
  *@argv: arguement array
- *Return: 0 success value
+ *Return: 0 success
  */
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	unsigned char elf_header[ELF_HEADER_SIZE];
-	size_t bytes_read;
+	Elf64_Ehdr elf_header;
 
 	if (argc != 2)
 	{
-		printf("Usage: elf_header elf_filename\n");
-		return (1);
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(1);
 	}
+
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		printf("Could not open file '%s'\n", argv[1]);
-		return (1);
+		fprintf(stderr, "Could not open file '%s'\n", argv[1]);
+		exit(1);
 	}
-	if (lseek(fileno(fp), 0, SEEK_SET) < 0)
+
+	if (lseek(fileno(fp), 0, SEEK_SET) != 0)
 	{
-		printf("Could not seek to start of file\n");
-		return (1);
+		perror("lseek");
+		exit(1);
 	}
-	bytes_read = read(fileno(fp), elf_header, ELF_HEADER_SIZE);
-	if (bytes_read != ELF_HEADER_SIZE)
+
+	if (read(fileno(fp), &elf_header, sizeof(elf_header)) != sizeof(elf_header))
 	{
-		printf("Could not read ELF header\n");
-		return (1);
+		perror("read");
+		exit(1);
 	}
-	printf("Magic: 0x%x\n", elf_header[0] << 8 | elf_header[1]);
-	printf("Class: %d\n", elf_header[4]);
-	printf("Data: %d\n", elf_header[5]);
-	printf("Version: %d\n", elf_header[6] << 8 | elf_header[7]);
-	printf("OS/ABI: %d\n", elf_header[8] << 8 | elf_header[9]);
-	printf("ABI Version: %d\n", elf_header[10] << 8 | elf_header[11]);
-	printf("Type: %d\n", elf_header[12] << 8 | elf_header[13]);
-	printf("Entry point address: 0x%x\n", elf_header[16] << 24 | elf_header[17] << 16 | elf_header[18] << 8 | elf_header[19]);
+	printf("ELF Header:\n");
+	printf("  Magic: 0x%08x\n", elf_header.e_ident[EI_MAG2]);
+	printf("  Class: %d\n", elf_header.e_ident[EI_CLASS]);
+	printf("  Data: %d\n", elf_header.e_ident[EI_DATA]);
+	printf("  Version: %d\n", elf_header.e_ident[EI_VERSION]);
+	printf("  OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
+	printf("  ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+	printf("  Type: %d\n", elf_header.e_type);
+	printf("  Entry point: 0x%lx\n", elf_header.e_entry);
 	fclose(fp);
 	return (0);
 }
