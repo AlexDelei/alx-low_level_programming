310d4023a5781b26c03cacf633245aadf760adc5
AlexDelei <alexdelei125@gmail.com>
2023-09-08 05:05:04 -0700

    Still weight 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,9 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(int code, const char *format, const char *arg);
 int main(int argc, char *argv[]);
-void print_elf_header_info(unsigned char *elf_header);
-void copyFile(const char *file_from, const char *file_to);
 void handle_error(int error_code, const char *file_path);
+void print_elf_header_info(unsigned char *elf_header);
 #endif
