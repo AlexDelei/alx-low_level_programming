166d83f84a00335730232774d072b500f96f16d6
AlexDelei <alexdelei125@gmail.com>
2023-09-08 05:10:28 -0700

    Still weight 1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,37 +14,49 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		handle_error(97, NULL);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		handle_error(98, file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		handle_error(99, file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
 	}
 	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			handle_error(99, file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-		handle_error(98, file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+	if (close(fd_from) == -1)
 	{
-		handle_error(100, (close(fd_from) == -1) ? file_from : file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	}
 	if (chmod(file_to, 0664) == -1)
 	{
-		handle_error(95, file_to);
+		dprintf(STDERR_FILENO, "Error: Can't set permissions for file %s\n", file_to);
+		exit(95);
 	}
 	return (0);
 }
