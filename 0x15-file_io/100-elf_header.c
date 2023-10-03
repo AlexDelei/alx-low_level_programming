d6453c8606290f0f177142508e7b86298b5c396a
AlexDelei <alexdelei125@gmail.com>
2023-10-02 22:52:46 -0700

    file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void display_elf_header_info(ElfHeader *header)
+{
+	printf("ELF Header:\n");
+	printf("  Magic:   %.2x %.2x %.2x %.2x\n", header->magic[0], header->magic[1], header->magic[2], header->magic[3]);
+	printf("  Class:                             %s\n", header->class == 1 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", header->data == 1 ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %u (current)\n", header->version);
+	printf("  OS/ABI:                            %u\n", header->os_abi);
+	printf("  ABI Version:                       %u\n", header->abi_version);
+	printf("  Type:                              0x%.4x\n", header->type);
+	printf("  Machine:                           0x%.4x\n", header->machine);
+	printf("  Version:                           0x%.8x\n", header->version2);
+	printf("  Entry point address:               0x%.16lx\n", (unsigned long)header->entry_point_address);
+}
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	const char *filename = argv[1];
+	int file;
+	ElfHeader elf_header;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <elf_filename>\n", argv[0]);
+		return (98);
+	}
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		perror("Failed to open file");
+		return (98);
+	}
+	if (read(file, &elf_header, sizeof(ElfHeader)) != sizeof(ElfHeader))
+	{
+		perror("Failed to read ELF header");
+		close(file);
+		return (98);
+	}
+	close(file);
+	display_elf_header_info(&elf_header);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,25 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+/**
+*
+*
+*
+*/
+typedef struct
+{
+	unsigned char magic[4];
+	uint8_t class;
+	uint8_t data;
+	uint8_t version;
+	uint8_t os_abi;
+	uint8_t abi_version;
+	uint8_t padding[7];
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version2;
+	uint64_t entry_point_address;
+} ElfHeader;
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
