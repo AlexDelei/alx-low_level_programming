0d0f72bf5f61201e940a36f253e947ed3179a28f
AlexDelei <alexdelei125@gmail.com>
2023-11-09 11:33:56 -0800

    0x17. C - Doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_dlistint - prints all the elements
+ *@h:pointer to strcut variable
+ *Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t nodeCount = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
