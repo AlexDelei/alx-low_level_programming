5ebc2dc1298c95cb1f5afaafbeb19e34155c3fe3
AlexDelei <alexdelei125@gmail.com>
2023-12-01 12:11:39 -0800

    Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));;
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 
 	if (ht == NULL || key == NULL || *key == '\0')
